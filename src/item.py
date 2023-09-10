b5b34d17d53956bb572133fad529577219a43c22
larmilch97 <pawlowa.sonya2011@gmail.com>
2023-09-10 13:11:24 +0400

    Task 1

--- src/item.py
+++ src/item.py
@@ -2,29 +2,20 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 1.0
     all = []
+    pay_rate = 1.0
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+        self.all.append(self)
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+        return self.price * self.quantity
 
-        :return: Общая стоимость товара.
-        """
-        pass
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+        return self.price
 
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,30 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def class_fixture_1():
+    item_test_1 = Item("Смартфон", 10000, 20)
+    return item_test_1
+
+
+@pytest.fixture
+def class_fixture_2():
+    item_test_2 = Item("Ноутбук", 20000, 5)
+    return item_test_2
+
+
+def test_calculate_total_price(class_fixture_1):
+    assert class_fixture_1.calculate_total_price() == 200000
+
+
+def test_calculate_total_price_2(class_fixture_2):
+    assert class_fixture_2.calculate_total_price() == 100000
+
+
+def test_apply_1(class_fixture_1):
+    assert class_fixture_1.apply_discount() == 10000
+
+
+def test_apply_2(class_fixture_2):
+    assert class_fixture_2.apply_discount() == 20000.0
